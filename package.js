55595d06af05b0ac8f2c93245fa6673f7a30f749
Ben Green <ben@latenightsketches.com>
2015-06-02 13:08:43 -0700

    Deprecation notice in package.js

--- package.js
+++ package.js
@@ -1,7 +1,7 @@
 Package.describe({
   name: 'numtel:publicsources',
-  summary: 'Create bundles in public directory for lazy-loading components',
-  version: '1.0.1',
+  summary: 'DEPRECATED: Switch to the new numtel:lazy-bundles package for latest release',
+  version: '1.0.2',
   git: 'https://github.com/numtel/meteor-publicsources.git'
 });
 
